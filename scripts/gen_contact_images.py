520c1523c6cdefe9f592d0ee30f3fd2705eb3b4a
Stanislav Lukeš <github@exyi.cz>
2023-09-20 12:35:34 +0200

    pair histograms with ilustration images

--- scripts/gen_contact_images.py
+++ scripts/gen_contact_images.py
@@ -29,26 +29,45 @@ def load(file, pdbid):
             print(file)
             cmd.load(file)
 
-def orient_pair(pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2):
+def orient_pair(pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, label_atoms: list[str]):
     cmd.hide("everything", f"%{pdbid}")
     pair_selection =f"%{pdbid} and ({residue_selection(chain1, nt1, ins1, alt1)} or {residue_selection(chain2, nt2, ins2, alt2)})"
     print(pair_selection)
     cmd.select("pair", pair_selection)
     cmd.show("sticks", "%pair")
     cmd.distance("pair_contacts", "%pair", "%pair", mode=2)
+    cmd.hide("labels", "pair_contacts")
     cmd.orient("%pair")
-    cmd.util.cbag(f"%pair")
-    cmd.show("lines", f"%{pdbid}")
-    cmd.label("%pair", "name")
-    cmd.clip("slab", 12, "%pair")
-    cmd.color("grey", f"%{pdbid} and not %pair")
+    cmd.util.cba("grey", f"%pair")
+    if "all" in label_atoms:
+        cmd.label("%pair", "name")
+    elif len(label_atoms) > 0:
+        cmd.label("%pair and (" + " or ".join([
+            (
+                f"({residue_selection(chain1, nt1, ins1, alt1)} and name \"{a[1:]}\")"
+                if a.startswith("A") else
+                f"({residue_selection(chain2, nt2, ins2, alt2)} and name \"{a[1:]}\")"
+                if a.startswith("B") else
+                f"name \"{a}\""
+            )
+            for a in label_atoms
+        ]) + ")", "name")
+    cmd.set("label_color", "black")
+    cmd.set("label_bg_color", "white", "%pair")
+    cmd.set("label_outline_color", "white")
+    cmd.set("label_size", 25)
+    # cmd.set("label_position", (0, 0, 0))
+    cmd.set("label_font_id", 7)
+    # cmd.show("lines", f"%{pdbid}")
+    # cmd.clip("slab", 12, "%pair")
+    # cmd.color("grey", f"%{pdbid} and not %pair")
 
-def make_pair_image(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2):
-    orient_pair(pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2)
-    cmd.png(output_file, width=640, height=640, ray=1)
+def make_pair_image(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, label_atoms: list[str]):
+    orient_pair(pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, label_atoms)
+    cmd.png(output_file, width=2560, height=1440, ray=1)
     print(f"Saved basepair image {output_file}")
 
-def process_group(pdbid, group: pl.DataFrame, output_dir: str):
+def process_group(pdbid, group: pl.DataFrame, output_dir: str, label_atoms: list[str]):
     pdbid = str(pdbid)
     load(None, pdbid)
     pdbdir = os.path.join(output_dir, pdbid)
@@ -59,18 +78,18 @@ def process_group(pdbid, group: pl.DataFrame, output_dir: str):
         alt1 = None if alt1 == '?' else alt1
         alt2 = None if alt2 == '?' else alt2
         output_file = os.path.join(pdbdir, f"{chain1}_{nt1}{ins1 or ''}{alt1 or ''}-{chain2}_{nt2}{ins2 or ''}{alt2 or ''}.png")
-        make_pair_image(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2)
+        make_pair_image(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, label_atoms)
 
-def make_images(df: pl.DataFrame, output_dir: str):
+def make_images(df: pl.DataFrame, output_dir: str, label_atoms: list[str]):
     for pdbid, group in sorted(df.groupby("pdbid")):
-        process_group(pdbid, group, output_dir)
+        process_group(pdbid, group, output_dir, label_atoms)
 
-def make_images_mp(df: pl.DataFrame, output_dir: str, threads: int):
+def make_images_mp(df: pl.DataFrame, output_dir: str, threads: int, label_atoms: list[str]):
     import multiprocessing
     multiprocessing.set_start_method("spawn")
     with multiprocessing.Pool(threads) as pool:
         processes = [
-            pool.apply_async(process_group, (pdbid, group, output_dir))
+            pool.apply_async(process_group, (pdbid, group, output_dir, label_atoms))
 
             for pdbid, group in sorted(df.groupby("pdbid"))
         ]
@@ -83,14 +102,15 @@ def main(argv):
     parser.add_argument("input", help="Input CSV file", nargs="+")
     parser.add_argument("--output-dir", "-o", required=True, help="Output directory")
     parser.add_argument("--threads", "-t", type=int, default=1, help="Number of threads to use")
+    parser.add_argument("--label-atoms", type=str, nargs="*", default=[], help="Atom names to label")
     args = parser.parse_args(argv)
 
     import pair_csv_parse
     df = pair_csv_parse.scan_pair_csvs(args.input).collect()
     if args.threads == 1:
-        make_images(df, args.output_dir)
+        make_images(df, args.output_dir, args.label_atoms)
     else:
-        make_images_mp(df, args.output_dir, args.threads)
+        make_images_mp(df, args.output_dir, args.threads, args.label_atoms)
 
 if __name__ == "__main__" or __name__ == "pymol":
     main(sys.argv[1:])
--- scripts/gen_histogram_plots.py
+++ scripts/gen_histogram_plots.py
@@ -1,7 +1,10 @@
 import itertools
 import subprocess
-from typing import Optional
-import polars as pl, numpy as np
+from typing import Optional, Union
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+import matplotlib.patheffects
+import polars as pl, numpy as np, numpy.typing as npt
 import os, sys, math, re
 import pairs
 import seaborn as sns
@@ -12,7 +15,7 @@ from dataclasses import dataclass
 
 
 bins_per_width = 80
-hist_kde = False
+hist_kde = True
 
 is_high_quality = pl.col("RNA-0-1.8") | pl.col("DNA-0-1.8")
 is_some_quality = pl.col("RNA-0-1.8") | pl.col("DNA-0-1.8") | pl.col("DNA-1.8-3.5") | pl.col("RNA-1.8-3.5")
@@ -28,8 +31,8 @@ subplots = (2, 2)
 resolutions = [
     ("DNA ≤3 Å", is_some_quality & is_rna.is_not() & (pl.col("resolution") <= 3)),
     ("RNA ≤3 Å", is_some_quality & is_rna & (pl.col("resolution") <= 3)),
-    ("DNA >3 Å", is_rna.is_not() & (pl.col("resolution") > 3)),
-    ("RNA >3 Å", is_rna & (pl.col("resolution") > 3)),
+    # ("DNA >3 Å", is_rna.is_not() & (pl.col("resolution") > 3)),
+    # ("RNA >3 Å", is_rna & (pl.col("resolution") > 3)),
     # ("DNA ≤1.8 Å", is_high_quality & is_rna.is_not()),
     # ("DNA 1.8 Å - 3.5 Å", is_med_quality & is_rna.is_not()),
     # ("≤1.8 Å", is_high_quality),
@@ -119,8 +122,12 @@ def get_label(col: str, pair_type: tuple[str, str]):
     
 def is_swapped(pair_bases):
     return pair_bases == "C-G"
-def format_pair_type(pair_type):
+def format_pair_type(pair_type, is_dna = False, is_rna=False):
     pair_kind, pair_bases = pair_type
+    if is_dna == True:
+        pair_bases = pair_bases.replace("U", "T")
+    elif is_rna == True:
+        pair_bases = pair_bases.replace("T", "U")
     if is_swapped(pair_bases):
         assert len(pair_kind) == 3
         return format_pair_type((f"{pair_kind[0]}{pair_kind[2]}{pair_kind[1]}", "-".join(reversed(pair_bases.split("-")))))
@@ -128,24 +135,35 @@ def format_pair_type(pair_type):
         return pair_kind + " " + pair_bases.replace("-", "")
     else:
         return pair_kind + " " + pair_bases
+    
+def crop_image(img: np.ndarray, padding = (0, 0, 0, 0)):
+    if len(img.shape) == 2:
+        img = img.reshape((*img.shape, 1))
+    xbitmap = np.any(img != 0, axis=(1, 2))
+    if not np.any(xbitmap):
+        return img
+    xlim = (np.argmax(xbitmap), len(xbitmap) - np.argmax(xbitmap[::-1]))
+    ybitmap = np.any(img != 0, axis=(0, 2))
+    ylim = (np.argmax(ybitmap), len(ybitmap) - np.argmax(ybitmap[::-1]))
 
+    xlim = (max(0, xlim[0] - padding[0]), min(img.shape[0], xlim[1] + padding[1]))
+    ylim = (max(0, ylim[0] - padding[2]), min(img.shape[1], ylim[1] + padding[3]))
 
+    return img[xlim[0]:xlim[1], ylim[0]:ylim[1], :]
 
-def make_histogram(df: pl.DataFrame, outdir: str, pair_type: tuple[str, str], h: HistogramDef, images = []):
-    title = f"{format_pair_type(pair_type)} {h.title}"
+def get_bounds(dataframes: list[pl.DataFrame], pair_type: tuple[str, str], h: HistogramDef):
     if h.min is not None:
         assert h.max is not None
-        xmin = h.min
-        xmax = h.max
+        return h.min, h.max
     else:
         datapoint_columns = [
-            df.filter(f)[col].drop_nulls().to_numpy()
-            for _, f in resolutions
+            df[col].drop_nulls().to_numpy()
+            for df in dataframes
             for col in h.columns
         ]
         datapoint_columns = [ c for c in datapoint_columns if len(c) > 0]
         if len(datapoint_columns) == 0:
-            raise ValueError(f"No datapoints for {title}")
+            raise ValueError(f"No datapoints for hisotgram {h.title} {pair_type}")
         all_datapoints = np.concatenate(datapoint_columns)
         mean = float(np.mean(all_datapoints))
         std = float(np.std(all_datapoints))
@@ -153,6 +171,22 @@ def make_histogram(df: pl.DataFrame, outdir: str, pair_type: tuple[str, str], h:
         pseudomax = min(mean + 3 * std, h.pseudomax or math.inf)
         xmin = min(pseudomin, float(np.min([ np.quantile(c, 0.02) for c in datapoint_columns ])))
         xmax = max(pseudomax, float(np.max([ np.quantile(c, 0.98) for c in datapoint_columns ])))
+        return xmin, xmax
+
+def get_histogram_ticksize(max, max_ticks = 8):
+    def it(max):
+        yield 1
+        yield 2
+        yield 5
+        yield 10
+        for x in it(max/10):
+            yield x*10
+    for ticksize in it(max):
+        if ticksize * max_ticks > max:
+            return ticksize
+
+def make_histogram_group(dataframes: list[pl.DataFrame], axes: list[plt.Axes], titles: list[str], pair_type: tuple[str, str], h: HistogramDef):
+    xmin, xmax = get_bounds(dataframes, pair_type, h)
 
     if h.bin_width is not None:
         bin_width = h.bin_width
@@ -165,52 +199,129 @@ def make_histogram(df: pl.DataFrame, outdir: str, pair_type: tuple[str, str], h:
     else:
         legend = [ get_label(col, pair_type) or "" for col in h.columns ]
 
-    if subplots:
-        main_fig, axes = plt.subplots(*subplots)
-        main_fig.tight_layout(pad=3.0)
-        axes = axes.reshape(len(resolutions))
-    else:
-        main_fig = None
-        axes = [ None ] * len(resolutions)
+    for df, ax, title in zip(dataframes, axes, titles):
+        ax.set(xlabel=h.axis_label, title=title)
+        ax.set_xlim(xmin, xmax)
+        nn_columns = [ c for c in h.columns if len(df[c].drop_nulls()) > 0 ]
 
-    for ax_i, (resolution_lbl, resolution_filter) in enumerate(resolutions):
-        dff = df.filter(resolution_filter)
-        plot: plt.Axes = axes[ax_i] or plt.gca()
-        plot.set(xlabel=h.axis_label,
-                 title=f"{resolution_lbl}" if subplots else f"{title} {resolution_lbl}"
-            )
-        plot.set_xlim(xmin, xmax)
-        nn_columns = [ c for c in h.columns if len(dff[c].drop_nulls()) > 0 ]
-
-        if len(dff) < 1 or len(nn_columns) == 0:
+        if len(df) < 1 or len(nn_columns) == 0:
             continue
 
-        dffs = dff[nn_columns]
-        print(bin_width, xmin, xmax, len(dffs), resolution_lbl, title)
-        renamed_columns = dffs.select(*[
+        dfs = df[nn_columns]
+        print(bin_width, xmin, xmax, len(dfs), title)
+        renamed_columns = dfs.select(*[
             pl.col(c).alias(l)
             for c, l in zip(h.columns, legend)
             if c in nn_columns
         ])
-        sns.histplot(data=renamed_columns.to_pandas(), binwidth=bin_width, kde=(hist_kde and len(dffs) >= 5), legend=True, ax=plot)
-        
-        print([ l for l, c in zip(legend, h.columns) if c in nn_columns ])
-        # plot.legend([ l for l, c in zip(legend, h.columns) if c in nn_columns ])
-        if not subplots:
-            yield save(plot.get_title(), outdir)
-    
+        sns.histplot(data=renamed_columns.to_pandas(), binwidth=bin_width, kde=(hist_kde and len(dfs) >= 5), legend=True, ax=ax)
+        ymax = ax.get_ylim()[1]
+        ax.set_yticks(np.arange(0, ymax, step=get_histogram_ticksize(ymax)))
+        if hist_kde:
+            for line in ax.lines:
+                x = line.get_xdata()
+                y = line.get_ydata()
+                peak = np.argmax(y)
+                ax.plot([ x[peak] ], [ y[peak] ], marker="o", color=line.get_color())
+                peak_fmt = f"{x[peak]:.0f}°" if "angle" in title.lower() else f"{x[peak]:.2f}"
+                ax.annotate(peak_fmt, (x[peak], y[peak]), xytext=(0, 5), textcoords="offset points", ha='center', va='bottom', color=line.get_color(), path_effects=[matplotlib.patheffects.withStroke(linewidth=3, foreground="white")], fontsize=8)
+
+def make_subplots(sp = subplots):
+    fig, sp = plt.subplots(*sp)
+    return fig, list(sp.reshape(-1))
+
+def make_bond_pages(df: pl.DataFrame, outdir: str, pair_type: tuple[str, str], hs: list[HistogramDef], images = None, highlights: Optional[list[pl.DataFrame]] = None):
+
+    pages: list[tuple[Figure, list[Axes]]] = [ make_subplots(subplots) for _ in resolutions ]
+    titles = [ f"{format_pair_type(pair_type, is_dna=('DNA' in resolution_lbl))} {resolution_lbl}" for resolution_lbl, _ in resolutions ]
+    print(titles)
+    for p, title in zip(pages, titles):
+        fig, _ = p
+        # fig.tight_layout(pad=3.0)
+        fig.suptitle(title)
+    dataframes = [ df.filter(resolution_filter) for _, resolution_filter in resolutions ]
+
+    for i, h in enumerate(hs):
+        make_histogram_group(dataframes, [ p[1][i+1] for p in pages ], [h.title] * len(dataframes), pair_type, h)
+    if images is not None:
+        for p, img, highlight in zip(pages, images, highlights or itertools.repeat(None)):
+            if img is None:
+                continue
+            ax = p[1][0]
+            img_data=crop_image(plt.imread(img), padding=(0, 30, 0, 0))
+            print(f"image {img} {img_data.shape}")
+            ax.imshow(img_data)
+            # ax.annotate("bazmek", (0, 1))
+            if highlight is not None:
+                def fmt_ins(ins):
+                    if not ins or ins == '\0' or ins == ' ':
+                        return ""
+                    else:
+                        return ".ins" + ins
+                def fmt_alt(alt):
+                    if not alt or alt == '?':
+                        return ""
+                    else:
+                        return ".alt" + alt
+                chain1 = f"chain {highlight[0, 'chain1']} "
+                chain2 = f"chain {highlight[0, 'chain2']} " if highlight[0, 'chain2'] != highlight[0, 'chain1'] else ""
+                address = f"{highlight[0, 'pdbid']}: {chain1}{highlight[0, 'res1']}{highlight[0, 'nr1']}{fmt_ins(highlight[0, 'ins1'])}{fmt_alt(highlight[0, 'alt1'])} · · · {chain2}{highlight[0, 'res2']}{highlight[0, 'nr2']}{fmt_ins(highlight[0, 'ins2'])}{fmt_alt(highlight[0, 'alt2'])}"
+                ax.text(0.5, 0, f"{address} ({highlight[0, 'resolution']:.1f} Å)", transform=ax.transAxes, horizontalalignment="center")
+            # ax.legend("bazmek")
+            ax.axis("off")
+    if highlights is not None:
+        for p, highlight in zip(pages, highlights):
+            if highlight is None or len(highlight) == 0:
+                continue
+
+            for ax, h in zip(p[1][1:], histogram_defs):
+                for col_i, col in enumerate(h.columns):
+                    if highlight[0, col] is not None:
+                        ax.plot([ float(highlight[0, col]) ], [ 0 ], marker="o", color=f"C{col_i}")
+
+    for p, title in zip(pages, titles):
+        fig, axes = p
+        # fig.tight_layout(pad=3.0)
+        fig.suptitle(title)
+        yield save(title, outdir)
+
+def make_resolution_comparison_page(df: pl.DataFrame, outdir: str, pair_type: tuple[str, str], h: HistogramDef, images = []):
+    title = f"{format_pair_type(pair_type)} {h.title}"
+
+    dataframes = [ df.filter(resolution_filter) for _, resolution_filter in resolutions ]
+
+    if subplots:
+        titles = [ f"{resolution_lbl}" for resolution_lbl, _ in resolutions ]
+        main_fig, axes = plt.subplots(*subplots)
+        main_fig.tight_layout(pad=3.0)
+        axes = list(axes.reshape(-1))
+        assert len(axes) == len(resolutions) + len(images)
+        for ax_i, img in enumerate(images):
+            ax_i += len(resolutions)
+            axes[ax_i].imshow(plt.imread(img))
+    else:
+        titles = [ f"{title} {resolution_lbl}" for resolution_lbl, _ in resolutions ]
+        main_fig = None
+        axes = [ plt.gca() for _ in resolutions ]
+    make_histogram_group(dataframes, axes, titles, pair_type, h)
+
     if subplots:
         assert main_fig is not None
         main_fig.suptitle(title)
         yield save(title, outdir)
+    else:
+        for ax_i, (resolution_lbl, resolution_filter) in enumerate(resolutions):
+            yield save(axes[ax_i].get_title(), outdir)
 
 
 def save(title, outdir):
     os.makedirs(outdir, exist_ok=True)
-    plt.savefig(os.path.join(outdir, title + ".pdf"))
+    pdf=os.path.join(outdir, title + ".pdf")
+    plt.savefig(pdf, dpi=300)
     plt.savefig(os.path.join(outdir, title + ".png"))
     plt.close()
-    return os.path.join(outdir, title + ".pdf")
+    print(f"Wrote {pdf}")
+    return pdf
 
 def load_pair_table(file: str):
     df = pl.read_parquet(file) if file.endswith(".parquet") else pl.read_csv(file)
@@ -260,12 +371,30 @@ def calculate_stats(df: pl.DataFrame, pair_type):
     medians = [ np.median(c) if len(c) > 0 else None for c in cdata ]
     means = [ np.mean(c) if len(c) > 0 else None for c in cdata ]
     stds = [ np.std(c) if len(c) > 1 else None for c in cdata ]
-    datapoint_log_likelihood = np.sum([ kde.logpdf(df[c].fill_null(-1000000000).to_numpy()) if kde is not None else np.zeros(len(df)) for kde, c in zip(kdes, columns) ], axis=0)
-    nicest_basepair = int(np.argmax(datapoint_log_likelihood))
+    kde_mode_stds = [ None if kde_mode is None else np.sqrt(np.mean((c - kde_mode) ** 2)) for kde_mode, c in zip(kde_modes, cdata) ]
+    datapoint_mode_deviations = np.sum([
+        ((df[c] - kde_mode) ** 2 / kde_mode_std ** 2).fill_null(10).to_numpy()
+        for kde_mode, kde_mode_std, c in zip(kde_modes, kde_mode_stds, columns)
+        if kde_mode is not None and kde_mode_std
+    ] + [ [0] * len(df) ], axis=0)
+    datapoint_log_likelihood = np.sum([ kde.logpdf(df[c].fill_null(0).to_numpy()) if kde is not None else np.zeros(len(df)) for kde, c in zip(kdes, columns) ], axis=0)
+    # print("datapoint_mode_deviations:", datapoint_mode_deviations)
+    # print("datapoint_log_likelihood:", datapoint_log_likelihood)
+    # nicest_basepair = int(np.argmax(datapoint_log_likelihood))
+    score = -datapoint_mode_deviations
+    score = score - np.min(score) + 1
+    nicest_basepair = int(np.argmax(score))
+    nicest_basepairs = [
+        int(np.argmax(np.concatenate([ [0.1], score * df.select(r.alias("x"))["x"].fill_null(False).to_numpy()]))) - 1
+        for _, r in resolutions
+    ]
+    print(f"{nicest_basepairs=}")
+    print(f"{nicest_basepair=} LL={datapoint_log_likelihood[nicest_basepair]} Σσ={datapoint_mode_deviations[nicest_basepair]} {next(df[nicest_basepair, columns].iter_rows())}")
     return {
         "count": len(df),
-        "nicest_bp": str(next(df[nicest_basepair, ["pdbid", "model", "chain1", "res1", "nr1", "ins1", "alt1", "chain2", "res2", "n r2", "ins2", "alt2"]].iter_rows())),
+        "nicest_bp": str(next(df[nicest_basepair, ["pdbid", "model", "chain1", "res1", "nr1", "ins1", "alt1", "chain2", "res2", "nr2", "ins2", "alt2"]].iter_rows())),
         "nicest_bp_index": nicest_basepair,
+        "nicest_bp_indices": nicest_basepairs,
         "hb_0_label": get_label("hb_0_length", pair_type),
         "hb_1_label": get_label("hb_1_length", pair_type),
         "hb_2_label": get_label("hb_2_length", pair_type),
@@ -278,23 +407,33 @@ def calculate_stats(df: pl.DataFrame, pair_type):
     }
 
 def create_pair_image(row: pl.DataFrame, output_dir: str, pair_type: tuple[str,str]) -> str:
+    if len(row) == 0:
+        return None
     os.makedirs(os.path.join(output_dir, "img"), exist_ok=True)
     row.write_parquet(os.path.join(output_dir, "img", f"nicest.parquet"))
     pdbid = row["pdbid"]
-    p = subprocess.run([
+    label_atoms = list(itertools.chain(*[ (x, y) for (_, x, y, _) in pairs.hbonding_atoms[pair_type] ]))
+    command = [
         "pymol", "-cq",
-        os.path.join(os.path.basename(__file__), "gen_contact_images.py"),
+        os.path.join(os.path.dirname(__file__), "gen_contact_images.py"),
         "--",
         os.path.join(output_dir, "img", f"nicest.parquet"),
+        "--label-atoms", *label_atoms,
         f"--output-dir={os.path.join(output_dir, 'img')}",
-    ], capture_output=True)
+    ]
+    p = subprocess.run(command, capture_output=True)
+    if p.returncode != 0:
+        print(p.stdout.decode('utf-8'))
+        print(p.stderr.decode('utf-8'))
+        raise ValueError(f"PyMOL failed with code {p.returncode}")
     output_lines = p.stdout.decode('utf-8').splitlines()
     for l in output_lines:
         if m := re.match(r"^Saved basepair image (.*)$", l):
             return m.group(1)
-        
+    print(*command)
     print(p.stdout.decode('utf-8'))
-    raise ValueError("Could not find PyMOL generated image file")
+    print(p.stderr.decode('utf-8'))
+    raise ValueError(f"Could not find PyMOL generated image file")
 
 def main(argv):
     import argparse
@@ -318,24 +457,32 @@ def main(argv):
         print(file)
         print(df[["hb_0_length", "hb_1_length", "hb_2_length"]].describe())
 
-        nicest_bp = None
-        # for resolution_cutoff in [1.8, 2.2, 3.5]:
-        #     dff = df.filter(is_some_quality).filter(pl.col("resolution") <= resolution_cutoff).filter(pl.col("hb_0_length").is_not_null() | pl.col("hb_1_length").is_not_null() | pl.col("hb_2_length").is_not_null())
-        #     if len(dff) == 0:
-        #         continue
-        #     statistics.append({
-        #         "pair": pair_type[1],
-        #         "pair_type": pair_type[0],
-        #         "resolution_cutoff": resolution_cutoff,
-        #         **calculate_stats(dff, pair_type),
-        #     })
-        #     if "nicest_bp_index" in statistics[-1]:
-        #         nicest_bp = statistics[-1]["nicest_bp_index"]
+        nicest_bps: Optional[list[int]] = None
+        for resolution_cutoff in [1.8, 2.2, 3.0]:
+            dff = df.filter(is_some_quality).filter(pl.col("resolution") <= resolution_cutoff).filter(pl.col("hb_0_length").is_not_null() | pl.col("hb_1_length").is_not_null() | pl.col("hb_2_length").is_not_null())
+            if len(dff) == 0:
+                continue
+            statistics.append({
+                "pair": pair_type[1],
+                "pair_type": pair_type[0],
+                "resolution_cutoff": resolution_cutoff,
+                **calculate_stats(dff, pair_type),
+            })
+            if "nicest_bp_indices" in statistics[-1]:
+                nicest_bps = statistics[-1]["nicest_bp_indices"]
+                del statistics[-1]["nicest_bp_indices"]
 
+        print(nicest_bps, len(dff))
+        # output_files = [
+        #     f
+        #     for h in histogram_defs
+        #     for f in make_resolution_comparison_page(df, args.output_dir, pair_type, h, images= [ create_pair_image(df[nicest_bp], args.output_dir, pair_type) ] if nicest_bp is not None else [])
+        # ]
         output_files = [
-            f
-            for h in histogram_defs
-            for f in make_histogram(df, args.output_dir, pair_type, h, images= [ create_pair_image(df[nicest_bp], args.output_dir, pair_type) ] if nicest_bp is not None else [])
+            f for f in make_bond_pages(df, args.output_dir, pair_type, histogram_defs,
+                                       images= [ create_pair_image(dff[bp], args.output_dir, pair_type) if bp >= 0 else None for bp in nicest_bps ] * len(resolutions) if nicest_bps is not None else [],
+                                       highlights=[ dff[bp] if bp >= 0 else None for bp in nicest_bps ] if nicest_bps is not None else []
+            )
         ]
         results.append({
             "count": len(df),
