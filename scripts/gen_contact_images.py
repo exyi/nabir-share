96f6e5871045673ba65ef1bac29b96880dc5e984
Stanislav Lukeš <github@exyi.cz>
2024-01-27 12:56:52 +0100

    contact img: webm(VP9) videos with alpha channel

--- scripts/gen_contact_images.py
+++ scripts/gen_contact_images.py
@@ -1,5 +1,6 @@
 import itertools
 import tempfile
+import time
 from typing import Optional
 import pymol
 from pymol import cmd
@@ -48,6 +49,12 @@ def transform_to_camera_space(coords):
     return camera_coords
 
 def rotate_to_y_axis(bond1, bond2):
+    if cmd.get_coords(bond1) is None:
+        print(f"WARNING: {bond1=} does not exist")
+        return
+    if cmd.get_coords(bond2) is None:
+        print(f"WARNING: {bond2=} does not exist")
+        return
     coord1 = transform_to_camera_space(cmd.get_coords(bond1)[0])
     coord2 = transform_to_camera_space(cmd.get_coords(bond2)[0])
     # rotate around z-axis such that coord1 is right above coord2
@@ -72,6 +79,14 @@ def flip_image_to_order(orient_updown = False, orient_left_phosphate_up = False,
     natom_l = get_n_atom("%leftnt")
 
     # main nucleotide should be on the left
+    leftC1 = "%pair and (not %rightnt) and (name C1')"
+    rightC1 = "%rightnt and (name C1')"
+    if cmd.get_coords(leftC1) is None:
+        print(f"WARNING: {leftC1=} does not exist")
+        return
+    if cmd.get_coords(rightC1) is None:
+        print(f"WARNING: {rightC1=} does not exist")
+        return
     rightC1_A = transform_to_camera_space(cmd.get_coords("%rightnt and (name C1')")[0])
     leftC1_A = transform_to_camera_space(cmd.get_coords("%leftnt and (name C1')")[0])
     if rightC1_A[0] < leftC1_A[0]:
@@ -258,7 +273,9 @@ class BPArgs:
     # label_atoms: list[str]
     standard_orientation: bool
     movie: int
+    movie_format: str
     incremental: bool
+    incremental_max_age: int
     skip_bad: bool
 
 def make_pair_image(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, bpargs: BPArgs, hbonds: Optional[list[tuple[str, str, str, str]]], pair_type: Optional[pair_defs.PairType]):
@@ -266,6 +283,25 @@ def make_pair_image(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, in
     orient_pair(pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, standard_orientation=bpargs.standard_orientation, hbonds=hbonds, pair_type=pair_type)
     cmd.png(output_file, width=2560, height=1440, ray=1)
     print(f"Saved basepair image {output_file}")
+
+def run_ffmpeg(output_file, input_dir, *args):
+    if os.path.exists(output_file) and output_file != "/dev/null":
+        os.remove(output_file)
+
+    ffmpeg_result = subprocess.run([
+        "ffmpeg",
+        "-framerate", "24",
+        "-pattern_type", "glob",
+        "-i", "./*.png",
+        *args,
+        os.path.abspath(output_file)
+    ], capture_output=True, cwd=input_dir)
+    if ffmpeg_result.returncode != 0:
+        print("ffmpeg failed:", " ".join(ffmpeg_result.args))
+        print(ffmpeg_result.stdout.decode("utf-8"))
+        print(ffmpeg_result.stderr.decode("utf-8"))
+        raise Exception("ffmpeg failed")
+    return ffmpeg_result
 
 def make_pair_rot_movie(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, bpargs: BPArgs, hbonds: Optional[list[tuple[str, str, str, str]]], pair_type: Optional[pair_defs.PairType]):
     length = bpargs.movie
@@ -295,26 +331,26 @@ def make_pair_rot_movie(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2
             try:
                 # cmd.mpng(output_file + ".pngdir/", mode=2, width=1920, height=1080)
                 cmd.mpng(output_file + ".pngdir/", mode=2, width=1280, height=720)
-                if os.path.exists(output_file):
-                    os.remove(output_file)
-                ffmpeg_result = subprocess.run([
-                    "ffmpeg",
-                    "-framerate", "30",
-                    "-pattern_type", "glob",
-                    "-i", output_file + ".pngdir/*.png",
-                    "-c:v", "libx264",
-                    "-pix_fmt", "yuv420p",
-                    output_file
-                ], capture_output=True)
-                if ffmpeg_result.returncode != 0:
-                    print("ffmpeg failed")
-                    print(ffmpeg_result.stdout.decode("utf-8"))
-                    print(ffmpeg_result.stderr.decode("utf-8"))
+                if bpargs.movie_format == "mp4":
+                    # mp4 H.264
+                    run_ffmpeg(output_file + ".mp4", output_file + ".pngdir",
+                        "-framerate", "24",
+                        "-pattern_type", "glob",
+                        "-i", output_file + ".pngdir/*.png",
+                        "-c:v", "libx264",
+                        "-pix_fmt", "yuv420p")
+                if bpargs.movie_format == "webm":
+                    # webm with alpha, VP9 twopass
+                    vp9opt = "-c:v libvpx-vp9 -auto-alt-ref 1 -lag-in-frames 25 -quality good -speed 0 -metadata:s:v:0 alpha_mode=1 -crf 45 -b:v 0".split(" ")
+                    run_ffmpeg("/dev/null", output_file + ".pngdir",
+                        *vp9opt, "-pass", "1", "-an", "-f", "null")
+                    run_ffmpeg(output_file + ".webm", output_file + ".pngdir",
+                        *vp9opt, "-pass", "2", "-an")
             finally:
                 shutil.rmtree(output_file + ".pngdir")
         else:
             from pymol import movie
-            movie.produce(output_file, mode="draw", quality=96, width=1280, height=720, encoder="ffmpeg")
+            movie.produce(output_file + ".mp4", mode="draw", quality=96, width=1280, height=720, encoder="ffmpeg")
         print("movie produced")
     finally:
         cmd.mclear()
@@ -346,31 +382,41 @@ def process_group(pdbid, group: pl.DataFrame, output_dir: str, bpargs: BPArgs):
     os.makedirs(pdbdir, exist_ok=True)
     type_column = group["type"] if "type" in group.columns else itertools.repeat(None)
     for pair_type, res1, chain1, nt1, ins1, alt1, res2, chain2, nt2, ins2, alt2 in zip(type_column, group["res1"], group["chain1"], group["nr1"], group["ins1"], group["alt1"], group["res2"], group["chain2"], group["nr2"], group["ins2"], group["alt2"]):
-        ins1 = None if ins1 == ' ' else ins1
-        ins2 = None if ins2 == ' ' else ins2
-        alt1 = None if alt1 == '?' else alt1
-        alt2 = None if alt2 == '?' else alt2
-        if pair_type:
-            pt = pair_defs.PairType.create(pair_type, pair_defs.map_resname(res1), pair_defs.map_resname(res2))
-            hbonds = pair_defs.get_hbonds(pt)
-            hbonds = [ b for b in hbonds if not pair_defs.is_bond_hidden(pt, b) ]
-        else:
-            hbonds = None
-            pt = None
+        try:
+            ins1 = None if ins1 == ' ' else ins1
+            ins2 = None if ins2 == ' ' else ins2
+            alt1 = None if alt1 == '?' else alt1
+            alt2 = None if alt2 == '?' else alt2
+            if pair_type:
+                pt = pair_defs.PairType.create(pair_type, pair_defs.map_resname(res1), pair_defs.map_resname(res2))
+                hbonds = pair_defs.get_hbonds(pt)
+                hbonds = [ b for b in hbonds if not pair_defs.is_bond_hidden(pt, b) ]
+            else:
+                hbonds = None
+                pt = None
 
-        output_file = os.path.join(pdbdir, f"{chain1}_{nt1}{ins1 or ''}{alt1 or ''}-{chain2}_{nt2}{ins2 or ''}{alt2 or ''}")
-        if bpargs.incremental and os.path.exists(output_file + ".png"):
-            if bpargs.movie == 0 or os.path.exists(output_file + ".mp4"):
-                print(f"Skipping {output_file} because it already exists")
-                continue
+            output_file = os.path.join(pdbdir, f"{chain1}_{nt1}{ins1 or ''}{alt1 or ''}-{chain2}_{nt2}{ins2 or ''}{alt2 or ''}")
+            if bpargs.incremental and os.path.exists(output_file + ".png"):
+                if bpargs.movie == 0 or os.path.exists(output_file + "." + bpargs.movie_format):
+                    age = os.path.getctime(output_file + ".png")
+                    if bpargs.movie:
+                        age = min(age, os.path.getctime(output_file + "." + bpargs.movie_format))
+                    age = (time.time() - age) / 3600 / 24
+                    if age < bpargs.incremental_max_age:
+                        print(f"Skipping {output_file} because it already exists (age={age:.1f})")
+                        continue
+                    else:
+                        print(f"Recreating {output_file} because it is too old (age={age:.1f} > {bpargs.incremental_max_age})")
 
-        load_if_needed()
-        make_pair_image(output_file + ".png", pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, bpargs, hbonds, pt)
-        # save_str(output_file + ".xyz", cmd.exporting.get_xyzstr("%pair"))
-        # save_str(output_file + ".cif", cmd.exporting.get_cifstr("%pair"))
-        # save_str(output_file + ".pdb", cmd.exporting.get_pdbstr("%pair"))
-        # save_str(output_file + ".sdf", cmd.exporting.get_sdfstr("%pair"))
-        make_pair_rot_movie(output_file + ".mp4", pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, bpargs, hbonds, pt)
+            load_if_needed()
+            make_pair_image(output_file + ".png", pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, bpargs, hbonds, pt)
+            # save_str(output_file + ".xyz", cmd.exporting.get_xyzstr("%pair"))
+            # save_str(output_file + ".cif", cmd.exporting.get_cifstr("%pair"))
+            # save_str(output_file + ".pdb", cmd.exporting.get_pdbstr("%pair"))
+            # save_str(output_file + ".sdf", cmd.exporting.get_sdfstr("%pair"))
+            make_pair_rot_movie(output_file, pdbid, chain1, nt1, ins1, alt1, chain2, nt2, ins2, alt2, bpargs, hbonds, pt)
+        except Exception as e:
+            print(f"ERROR in {pdbid} {chain1}-{res1}{alt1}{ins1}...{chain2}-{res2}{alt2}{ins2}: {e}")
 
     cmd.reinitialize()
 
@@ -395,9 +441,19 @@ def make_images_mp(df: pl.DataFrame, output_dir: str, threads: int, affinity: Op
     #     affinities = [ [a] for a in affinity ]
     # else:
     #     affinities = [affinity] * threads
+
+    print(f"Running parallel version, {threads=}")
     import multiprocessing
     multiprocessing.set_start_method("spawn")
     with multiprocessing.Pool(threads, initializer=process_init, initargs=(niceness,affinity)) as pool:
+        children = multiprocessing.active_children()
+        assert len(children) == threads
+        if affinity and len(affinity) == threads:
+            for child, aff in zip(children, affinity):
+                assert child.pid is not None and child.pid != os.getpid()
+                os.sched_setaffinity(child.pid, [aff])
+
+            print(f"set pinned per-process affinity - {', '.join(f'{child.pid}: {os.sched_getaffinity(child.pid)}' for child in children)}")
         processes = [
             pool.apply_async(process_group, (pdbid, group, output_dir, bpargs))
 
@@ -417,13 +473,15 @@ def main(argv):
     parser.add_argument("--standard-orientation", type=eval, default=False, help="When set to true, orient the base pair such that the first nucleotide is always left and the N1/N9 - C1' is along the y-axis (N is above C)")
     parser.add_argument("--label-atoms", type=str, nargs="*", default=[], help="Atom names to label")
     parser.add_argument("--movie", type=int, default=0, help="If not zero, produce a rotating animation of the base pair")
+    parser.add_argument("--movie-format", type=str, default="webm", help="webm (VP9 with alpha) or mp4 (H.264)")
     parser.add_argument("--incremental", type=bool, default=False, help="Generate the image/video only if it does not exist yet")
+    parser.add_argument("--incremental-max-age", type=int, default=False, help="Maximum age of the image/video to be considered done, in days")
     parser.add_argument("--skip-bad", type=bool, default=False, help="Skip basepairs with bad or missing parameters")
     args = parser.parse_args(argv)
 
     import pair_csv_parse
     df = pair_csv_parse.scan_pair_csvs(args.input).collect()
-    bpargs = BPArgs(args.standard_orientation, args.movie, args.incremental, args.skip_bad)
+    bpargs = BPArgs(args.standard_orientation, args.movie, args.movie_format, args.incremental, args.incremental_max_age, args.skip_bad)
     threads = args.threads
     if threads is None and args.cpu_affinity is not None:
         threads = len(args.cpu_affinity)
@@ -434,7 +492,7 @@ def main(argv):
         process_init(args.niceness, args.cpu_affinity)
         make_images(df, args.output_dir, bpargs)
     else:
-        make_images_mp(df, args.output_dir, args.threads, args.cpu_affinity, args.niceness, bpargs)
+        make_images_mp(df, args.output_dir, threads, args.cpu_affinity, args.niceness, bpargs)
 
 if __name__ == "__main__" or __name__ == "pymol":
     main(sys.argv[1:])
